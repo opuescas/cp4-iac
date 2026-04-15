19788b338de72854a1bd828aa7c76806c749c874
Omar Puescas <opuescas@soaint.com>
2026-04-14 21:49:27 -0500

    chore: update ACE dashboard configuration and APIC cluster storage settings

--- stages/02-instances/z_auto_ACE_int_dashboard.tf
+++ stages/02-instances/z_auto_ACE_int_dashboard.tf
@@ -1,5 +1,5 @@
 # AUTO-GENERATED from yamls/ACE/int-dashboard.yaml
-# YAML-HASH: 19f55270e24a8d672ca75b4b8182971a
+# YAML-HASH: 3540dab259a12d5d0b82255fa594a3aa
 resource "kubernetes_manifest" "gen_ACE_int_dashboard" {
   manifest = yamldecode(file("${path.module}/../../yamls/ACE/int-dashboard.yaml"))
 }
